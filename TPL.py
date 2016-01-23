d86924618ec33a39ff0f21092cf7084f0224255b
Chenye Charlie Zhu <chenye@stanford.edu>
2016-01-23 07:41:50 -0800

    Fixed a bug in TPL algorithm

--- TPL.py
+++ TPL.py
@@ -22,11 +22,14 @@ def TPL(persons, houses, owner, preferences):
             current_person = order[0]
             house_wanted = aspiration_house(preferences[current_person], assigned_houses)
             cycle_found = False
+
             if current_person in current_tenants:
                 cycle_person = current_person
                 cycle_house = house_wanted
                 sequence = [cycle_person, cycle_house]
-                while cycle_house in owner.keys() and owner[cycle_house] not in sequence:
+                while assignment[cycle_person] is None \
+                        and cycle_house in owner.keys() \
+                        and owner[cycle_house] not in sequence:
                     cycle_person = owner[cycle_house]
                     cycle_house = aspiration_house(preferences[cycle_person], assigned_houses)
                     sequence.append(cycle_person)
@@ -42,7 +45,9 @@ def TPL(persons, houses, owner, preferences):
                     cycle_found = True
 
             if not cycle_found:
-                if house_wanted not in owner or assignment[owner[house_wanted]] is not None or owner[house_wanted] == current_person:
+                if house_wanted not in owner \
+                        or assignment[owner[house_wanted]] is not None \
+                        or owner[house_wanted] == current_person:
                     assignment[current_person] = house_wanted
                     assigned_houses[house_wanted] = True
                     order.remove(current_person)
@@ -73,7 +78,13 @@ def testTPL01():
     preferences = {p1: (h1, h2, h3), p2: (h1, h3, h2), p3: (h2, h3, h1)}
 
     statistics_count, housing_assignments = TPL(persons, houses, owner, preferences)
-    print housing_assignments[(p1, p2, p3)]
+
+    specific_case = housing_assignments[(p1, p2, p3)]
+    assert(specific_case[p1] == h1)
+    assert(specific_case[p2] == h3)
+    assert(specific_case[p3] == h2)
+
+    print "Test Case 1 : ", specific_case
 
 def testTPL02():
     p1 = 'Gwen'
@@ -91,7 +102,14 @@ def testTPL02():
     owner = {h3: p3}
     preferences = {p1: (h2, h1, h3, h4), p2: (h2, h3, h1, h4), p3: (h1, h2, h3, h4), p4: (h3, h2, h1, h4)}
     statistics_count, housing_assignments = TPL(persons, houses, owner, preferences)
-    print housing_assignments[(p1, p2, p3, p4)]
+
+    specific_case = housing_assignments[(p1, p2, p3, p4)]
+    assert (specific_case[p1] == h2)
+    assert (specific_case[p2] == h3)
+    assert (specific_case[p3] == h1)
+    assert (specific_case[p4] == h4)
+
+    print "Test Case 2 :", specific_case
 
 def testTPL03():
     p1 = 'Madeline'
@@ -108,9 +126,15 @@ def testTPL03():
     houses = (h1, h2, h3, h4)
     owner = {h1: p1,h2: p2, h3: p3, h4: p4}
     preferences = {p1: (h2, h1, h3, h4), p2: (h4, h3, h1, h2), p3: (h4, h1, h3, h2), p4: (h2, h1, h4, h3)}
-
     statistics_count, housing_assignments = TPL(persons, houses, owner, preferences)
-    print housing_assignments[(p1, p2, p3, p4)]
+
+    specific_case = housing_assignments[(p1, p2, p3, p4)]
+    assert (specific_case[p1] == h1)
+    assert (specific_case[p2] == h4)
+    assert (specific_case[p3] == h3)
+    assert (specific_case[p4] == h2)
+
+    print "Test Case 3 :", specific_case
 
 def testTPL04():
     p1 = 'Gwen'
@@ -128,9 +152,14 @@ def testTPL04():
     owner = {h3: p3}
     preferences = {p1: (h2, h1, h3, h4), p2: (h2, h3, h1, h4), p3: (h3, h2, h1, h4), p4: (h3, h2, h1, h4)}
     statistics_count, housing_assignments = TPL(persons, houses, owner, preferences)
-    print housing_assignments[(p1, p2, p3, p4)]
-    # print statistics_count
 
+    specific_case = housing_assignments[(p1, p2, p3, p4)]
+    assert (specific_case[p1] == h2)
+    assert (specific_case[p2] == h1)
+    assert (specific_case[p3] == h3)
+    assert (specific_case[p4] == h4)
+
+    print "Test Case 4 :", specific_case
 
 testTPL01()
 testTPL02()
